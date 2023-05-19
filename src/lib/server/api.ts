ed8f457da447a122283a9b4b0d19c94c3c39549b
Soham S Gumaste <sgumas2@uic.edu>
2023-05-19 15:10:04 +0530

    Formatting Improvements (opinionated)

--- src/lib/server/api.ts
+++ src/lib/server/api.ts
@@ -17,32 +17,32 @@ export type Result = { error: boolean; message: string };
 /**
 	* @type LdapClient
 	* @description Placeholder till I get LdapJS types
+	* @todo Get real types for LdapJS
 	*/
 export type LdapClient = any;
 
 // Wait I'm supposed to hate OO?
 
 /**
- * @class ldap_class
- * Functions related to Ldap read/write
- * This class should *only* contain static methods
- */
+	* @class ldap_class
+	* Functions related to Ldap read/write
+	* This class should *only* contain static methods
+	*/
 class ldap_class {
 	private client: LdapClient;
 	private client_user: LdapClient;
 	private isBound: boolean;
 
 	/**
-	* BIND to LDAP here! Use the service account.
-	* Add event handlers for reconnection, errors.
-	*
-	* UPDATE:
-	* It is best practice to bind as the user to
-	* authenticate! `client` is our client for admin
-	* ops, client_user is for binding.
-	* NOTE:
-	* Connecting here makes the first load very slow!
-	*/
+		*
+		*
+		* UPDATE:
+		* It is best practice to bind as the user to
+		* authenticate! `client` is our client for admin
+		* ops, client_user is for binding.
+		* NOTE:
+		* Connecting here makes the first load very slow!
+		*/
 	constructor() {
 		console.log("Attempting LDAP contact...");
 		this.isBound = false;
@@ -65,6 +65,7 @@ class ldap_class {
 			url: [LDAP_URL],
 			reconnect: true
 		});
+		/** @todo Better Error Reporting */
 		cl.on('error', (err: any) => {
 			if (err !== null)
 				console.log(`LdapClient: ${err}`);
@@ -73,41 +74,45 @@ class ldap_class {
 	}
 
 	/**
-	 * @function validateUser
-	 * This function is the single source of truth
-	 * for User Authentication!
-	 * 
-	 * @param {string} user
-	 * @param {string} password
-	 * @return {Promise<Result>} 
-	 * @desc: This function should check if the username and password
-	 *           exist in the ActiveDirectory (interfaced with LDAP.js)
-	 */
+		* @function validateUser
+		* This function is the single source of truth
+		* for User Authentication!
+		*
+		* @param {string} user
+		* @param {string} password
+		* @return {Promise<Result>}
+		* @desc: This function should check if the username and password
+		*           exist in the ActiveDirectory (interfaced with LDAP.js)
+		*/
 	async validateUser(user: string, password: string): Promise<Result> {
 		console.log('validateUser called!');
 		console.log(`Bound? ${this.isBound}`);
 		// if (username === 'ACM' && password === 'testing') {
-		// 	return { error: 0, message: '' };
+		//	return { error: 0, message: '' };
 		// } else {
-		// 	return { error: 1, message: 'Error!' };
+		//	return { error: 1, message: 'Error!' };
 		// }
 		console.log(`Attempting to bind as user ${user}`);
 
 		let success = await util._bind(this.client_user, user, password);
 		console.log(`Returning Success ${success}`);
-		return {error: !success, message:''};
+		return { error: !success, message: '' };
 	}
 
 	/**
 	 * @function change_password
-	 * TODO: Implement this function!
-	 * Input: user: string, newpass: string
-	 * Expected: User's password is changed to newpass.
+	 * @todo: Implement this function!
+	 * @param {string} user
+	 * @param {string} newpass
+	 * @return {Promise<Result>}
+	 * @desc
+	 * User's password is changed to newpass.
 	 * Notes:
 	 * Need to BIND to LDAP with a service user (we already have one)
 	 * This function will need to consume a secret.
 	 * This **MUST** be done with an environment variable. (.env file)
-	 * Use instance variables in this class. Ideally, bind when the class is created.
+	 * Use instance variables in this class.
+	 * Ideally, bind when the class is created.
 	 * DO NOT ATTEMPT IF U DONT KNOW WHAT YOU ARE DOING
 	 */
 	change_password(user: string, newpass: string): Result {
@@ -121,23 +126,24 @@ class ldap_class {
  * Functions related to cookies and session management
  */
 class session_class {
-	// TODO: Implement
-	private cookie_tray: any;
-	private secret;
+
+	/**
+		* @desc
+		* Contains the JWT signing secret
+		* Leaking this will allow anyone to fake any user!
+		*/
+	private secret: Uint8Array;
 
 	constructor() {
-		this.cookie_tray = null;
 		this.secret = new TextEncoder().encode(TOKEN);
 	}
 
-	/*
-	 * @function @static create_session_string
-	 * Create "secure" string to identify the authenticated
-	 * user. Store it in a data store with an expiry (cookie-tray)
-	 * This is a username --> time-bound string mapping.
-	 * Some form of hash function should ideally be used.
-	 */
-	async create_session_string(username: string): string {
+	/**
+		* @function @static create_session_string
+		* @desc Issues a JWT to the user
+		* This function **must** be called only after authentication!
+		*/
+	async create_session_string(username: string): Promise<string> {
 		// return username === 'ACM' ? 'ABCXYZ69420' : username;
 		const jwt = await new jose.SignJWT({ username })
 			.setProtectedHeader({ alg: 'HS256' })
@@ -150,15 +156,13 @@ class session_class {
 		return jwt;
 	}
 
-	/*
-	 * @function get_session_string
-	 * Essentially Reverse @refer {create_session_string}
-	 * Must perform cookie validation/freshness check!
-	 * If cookie is stale, or non-existent return null
-	 * If cookie is valid, return username.
-	 * Scaling: This function will be called quite a lot of times.
-	 */
-	async get_session_string(cookie: string): string | null {
+	/**
+		* @function get_session_string
+		* @see {@link create_session_string}
+		* @desc Validates the JWT presented by the client
+		* Scaling: This function will be called quite a lot of times.
+		*/
+	async get_session_string(cookie: string): Promise<string | null> {
 		// return cookie === 'ABCXYZ69420' ? 'ACM' : null;
 		try {
 			const { payload } = await jose.jwtVerify(cookie, this.secret, {
@@ -166,12 +170,13 @@ class session_class {
 				audience: 'acmlug'
 			});
 			// console.log(`Header:${protectedHeader}\nPayload:${payload}`);
+			// @ts-ignore
 			return payload.username;
 		} catch (e) {
 			console.log('JWTverify failed!');
 			console.log(e);
 			console.log(cookie);
-			console.log(await cookie);
+			console.log(cookie);
 			return null;
 		}
 	}
